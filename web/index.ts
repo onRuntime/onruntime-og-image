3f80ce0dc75745f747fb5de130b2dbc1be4344f9
Antoine Kingue <etorefr@gmail.com>
2022-08-18 10:21:47 +0200

    🔧 update images options

--- web/index.ts
+++ web/index.ts
@@ -147,14 +147,14 @@ const markdownOptions: DropdownOption[] = [
 const imageLightOptions: DropdownOption[] = [
     { text: 'Vercel', value: 'https://assets.vercel.com/image/upload/front/assets/design/vercel-triangle-black.svg' },
     { text: 'Next.js', value: 'https://assets.vercel.com/image/upload/front/assets/design/nextjs-black-logo.svg' },
-    { text: 'Hyper', value: 'https://assets.vercel.com/image/upload/front/assets/design/hyper-color-logo.svg' },
+    { text: 'Expat Facilities', value: 'https://expatfacilities.co/static/images/logo/logo.svg' },
 ];
 
 const imageDarkOptions: DropdownOption[] = [
 
-    { text: 'Vercel', value: 'https://assets.vercel.com/image/upload/front/assets/design/vercel-triangle-white.svg' },
+    { text: 'onRuntime', value: 'https://onruntime.com/static/images/logo/onruntime.svg' },
     { text: 'Next.js', value: 'https://assets.vercel.com/image/upload/front/assets/design/nextjs-white-logo.svg' },
-    { text: 'Hyper', value: 'https://assets.vercel.com/image/upload/front/assets/design/hyper-bw-logo.svg' },
+    { text: 'Expat Facilities', value: 'https://expatfacilities.co/static/images/logo/logo.svg' },
 ];
 
 
