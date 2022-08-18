6486a573a4002348f504dd9c056226ce90fa4d47
Antoine Kingue <etorefr@gmail.com>
2022-08-18 10:21:29 +0200

    🔧 update default image

--- api/_lib/parser.ts
+++ api/_lib/parser.ts
@@ -53,12 +53,12 @@ function getArray(stringOrArray: string[] | string | undefined): string[] {
 function getDefaultImages(images: string[], theme: Theme): string[] {
     const defaultImage = theme === 'light'
         ? 'https://assets.vercel.com/image/upload/front/assets/design/vercel-triangle-black.svg'
-        : 'https://assets.vercel.com/image/upload/front/assets/design/vercel-triangle-white.svg';
+        : 'https://onruntime.com/static/images/logo/onruntime.svg';
 
     if (!images || !images[0]) {
         return [defaultImage];
     }
-    if (!images[0].startsWith('https://assets.vercel.com/') && !images[0].startsWith('https://assets.zeit.co/')) {
+    if (!images[0].startsWith('https://onruntime.com/static/images/') && !images[0].startsWith('https://tonightpass.com/static/')) {
         images[0] = defaultImage;
     }
     return images;
